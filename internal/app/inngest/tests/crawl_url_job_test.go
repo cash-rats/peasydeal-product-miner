eb299e16d9c94a141cdc1c9478f1ad71706154e8
Bryan Huang <huangchiheng@gmail.com>
2026-01-14 15:57:55 +0800

    Minor tweak

--- internal/app/inngest/tests/crawl_url_job_test.go
+++ internal/app/inngest/tests/crawl_url_job_test.go
@@ -62,7 +62,7 @@ func (s *CrawlURLJobTestSuite) TestSendCrawlerURLRequested() {
 		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
 
-		targetURL := "https://shopee.tw/%E3%80%90%E9%99%84%E7%99%BC%E7%A5%A8%E3%80%9124H%E6%A1%8C%E5%A2%8A-%E9%80%8F%E6%98%8E%E7%A3%A8%E9%82%8A-%E9%98%B2%E6%B0%B4%E9%98%B2%E6%B2%B9-%E5%AE%A2%E8%A3%BD%E8%A3%81%E5%89%AA-%E5%8E%9A%E5%BA%A65mm-%E5%A4%9A%E9%81%B8%E9%A4%90%E6%A1%8C%E5%A2%8A-%E5%9C%93%E6%A1%8C%E5%A2%8A%E8%8C%B6%E5%87%A0%E5%A2%8A-%E9%AB%98%E7%B4%9A%E6%84%9F%E5%8F%AF%E8%A3%81%E5%89%AA%E7%84%A1%E5%91%B3%E6%A1%8C%E5%A2%8A-%E9%98%B2%E7%87%99%E9%98%B2%E6%B2%B9-i.279216161.21065663294?extraParams=%7B%22display_model_id%22%3A158245746215%2C%22model_selection_logic%22%3A3%7D"
+		targetURL := "https://shopee.tw/Logitech-%E7%BE%85%E6%8A%80-LIFT%E4%BA%BA%E9%AB%94%E5%B7%A5%E5%AD%B8%E5%9E%82%E7%9B%B4%E6%BB%91%E9%BC%A0-i.48032592.20201505683?extraParams=%7B%22display_model_id%22%3A87667287725%2C%22model_selection_logic%22%3A3%7D&sp_atk=71e79cbe-a632-43b5-83be-4370357fd0a9&xptdk=71e79cbe-a632-43b5-83be-4370357fd0a9"
 		eventID, err := crawl.EventIDForURL(targetURL)
 		s.Require().NoError(err)
 
